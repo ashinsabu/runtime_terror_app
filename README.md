3e8878fd5dcef06caf7b847d597a8d121b401e7a
Ashin Sabu <ashin.sabu3@gmail.com>
2022-04-13 20:58:56 +0530

    Initialized project, file structure and configured backend

---
+++ README.md
@@ -0,0 +1,5 @@
+# runtime_terror_app
+
+A mobile application to solve various problems faced by the NCB
+
+
---
+++ android/app/src/main/kotlin/com/runtimeterror/runtime_terror_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.runtimeterror.runtime_terror_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
